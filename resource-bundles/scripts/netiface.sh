460750b35fe4c273f8d59268d2791da9ccbe4802
Larry Feigen <larry.feigen@oracle.com>
2021-11-04 11:54:11 -0400

    Create Hugo documentation branch

--- resource-bundles/scripts/netiface.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Copyright 2020, Oracle Corporation and/or its affiliates.  All rights reserved.
-# Licensed under the Universal Permissive License v 1.0 as shown at http://oss.oracle.com/licenses/upl.
-
-echo "script output"
